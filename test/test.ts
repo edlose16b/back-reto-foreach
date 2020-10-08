f7648ad8409261a9e6caf632a8d77e9efd78aadd
Edwin <edwin.cbalbin@gmail.com>
2020-10-08 02:53:25 -0500

    Presentacion

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,3 @@
-
 import assert from 'assert';
 import { calculatekgO2EmissionPerPerson } from './../app/utils';
 
@@ -14,27 +13,29 @@ import { calculatekgO2EmissionPerPerson } from './../app/utils';
 // Transport.create({ id: 9, name: 'Caminando', emission: 0 });
 
 describe('test calculate kgO2Emission per person', function () {
-    it('should return 0.33 when usas Metro con 2 trabajadores recorriendo 10 km en un viaje de ida y vuelta', function () {
+
+    
+    it('should return 0.33 when use Metro with 2 workers traveling 10 km in a round trip', function () {
         assert.strictEqual(calculatekgO2EmissionPerPerson(0.033, 2, 10, true), 0.33);
     });
 
-    it('should return 0.350 when usas Auto (Gasolina) con 6 trabajadores recorriendo 10 km en un viaje de ida', function () {
+    it('should return 0.350 when use Auto (Gasolina) with 6 workers traveling 10 km in a one-way trip', function () {
         assert.strictEqual(calculatekgO2EmissionPerPerson(0.21, 6, 10, false), 0.350);
     });
 
-    it('should return 0.780 when usas Bus Transantiafo con 1 trabajador recorriendo 10 km en un viaje de ida y vuelta', function () {
+    it('should return 0.780 when use Bus Transantiafo with 1 trabajador recorriendo 10 km in a round trip ida y vuelta', function () {
         assert.strictEqual(calculatekgO2EmissionPerPerson(0.039, 1, 10, true), 0.780);
     });
 
-    it('should return 111.600 when usas Avion (Chile)  con 3 trabajadores recorriendo 600 km en un viaje de ida y vuelta', function () {
+    it('should return 111.600 when use Avion (Chile)  with 3 workers traveling 600 km in a round trip', function () {
         assert.strictEqual(calculatekgO2EmissionPerPerson(0.279, 3, 600, true), 111.600);
     });
 
-    it('should return 0.710 when usas Metro con 2 trabajadores recorriendo 43 km en un viaje de ida', function () {
+    it('should return 0.710 when use Metro with 2 workers traveling 43 km in a one-way trip', function () {
         assert.strictEqual(calculatekgO2EmissionPerPerson(0.033, 2, 43, false), 0.710);
     });
 
-    it('should return 111.517 when usas Avion (Internacional) con 2 trabajadores recorriendo 623 km en un viaje de ida y vuelta', function () {
+    it('should return 111.517 when use Avion (Internacional) with 2 workers traveling 623 km in a round trip', function () {
         assert.strictEqual(calculatekgO2EmissionPerPerson(0.179, 2, 623, true), 111.517);
     });
 });
